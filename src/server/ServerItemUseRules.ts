87debbdea9f4749f0c08c9904660fdecb96e8ef3
Aben <5195693+rushairer@users.noreply.github.com>
2026-09-22 00:52:21 +0800

    parity 741: prevent generic item-use from bypassing brush timing

--- src/server/ServerItemUseRules.ts
+++ src/server/ServerItemUseRules.ts
@@ -56,7 +56,6 @@ export function isSupportedServerItemUseName(name: string, kind: ServerItemUseIn
     || name === 'powder_snow_bucket'
     || name === 'flint_and_steel'
     || name === 'shears'
-    || name === 'brush'
     || name === 'boat'
     || name.endsWith('_boat')
     || name.includes('minecart')
